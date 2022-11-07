c8f34b7ba7a72dfe8cb8b7dcd66edfce56f2d5bc
BriceBOKATOLA <91880016+BriceBOKATOLA@users.noreply.github.com>
2022-11-07 09:58:51 +0100

    bootstrap
    ajout bootstrap

--- inc/header.php
+++ inc/header.php
@@ -7,6 +7,8 @@
     <!-- ajout du react -->
     <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
     <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+     <!-- bootstrap  -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- ajout du css -->
     <link rel="stylesheet" href="assets/style.css"> 
     <title>La Manu Box</title>
