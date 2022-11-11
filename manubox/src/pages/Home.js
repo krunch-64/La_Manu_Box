7619d7e3bda139874723e7d74c56764472332880
Romain Bailhache <romain.bailhache@gmail.com>
2022-11-11 16:51:35 +0100

    push

--- manubox/src/pages/Home.js
+++ manubox/src/pages/Home.js
@@ -4,8 +4,8 @@ import Navigation from '../components/Navigation';
 const Home = () => {
     return (
         <div>
-            <Navigation />
             <h1>Bienvue sur La Manu Box</h1>
+            <Navigation />
         </div>
     );
 };
